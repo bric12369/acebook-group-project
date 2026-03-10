f0d1f62c74713e56a34e371f164d2a18684bb10f
mahmudur mahid <m.mahid@icloud.com>
2026-03-10 14:25:14 +0000

    added count number of likes functionality, and to check if a user has liked a post functionality

--- src/main/java/com/makersacademy/acebook/repository/LikeRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/LikeRepository.java
@@ -4,5 +4,6 @@ import com.makersacademy.acebook.model.Like;
 import org.springframework.data.repository.CrudRepository;
 
 public interface LikeRepository extends CrudRepository<Like, Long> {
-
+    long countByPostId(Long postId);
+    boolean existsByPostIdAndUserId(Long postId, Long userId);
 }
