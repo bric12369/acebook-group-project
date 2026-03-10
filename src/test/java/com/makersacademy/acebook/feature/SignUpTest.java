e5b78a0796b7dfc0015df9c7906d3dba7fcdf32e
= <=>
2026-03-10 16:40:13 +0000

    Fixing failing Playwright test for login functionality

--- src/test/java/com/makersacademy/acebook/feature/SignUpTest.java
+++ src/test/java/com/makersacademy/acebook/feature/SignUpTest.java
@@ -41,7 +41,6 @@ public class SignUpTest {
         page.locator("[name=email]").fill(email);
         page.locator("[name=password]").fill("P@55qw0rd");
         page.locator("[name=action]").click();
-        page.locator("[name=action]").filter(new Locator.FilterOptions().setHasText("Accept")).click();
         Locator greeting = page.locator("#greeting");
 
         assertThat(greeting).hasText("Signed in as " + email);
