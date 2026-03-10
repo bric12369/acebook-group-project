bfac3658587342ddac7345c6aa7df2f033965cc0
Barry <barryrichards05@gmail.com>
2026-03-10 13:13:30 +0000

    added post comment functionality with hard coded user id

--- src/main/java/com/makersacademy/acebook/controller/PostsController.java
+++ src/main/java/com/makersacademy/acebook/controller/PostsController.java
@@ -1,8 +1,14 @@
 package com.makersacademy.acebook.controller;
 
+import com.makersacademy.acebook.model.Comment;
 import com.makersacademy.acebook.model.Post;
+import com.makersacademy.acebook.model.User;
+import com.makersacademy.acebook.repository.CommentRepository;
 import com.makersacademy.acebook.repository.PostRepository;
+import com.makersacademy.acebook.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -11,12 +17,19 @@ import org.springframework.web.servlet.view.RedirectView;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class PostsController {
 
     @Autowired
     PostRepository repository;
+
+    @Autowired
+    CommentRepository commentRepository;
+
+    @Autowired
+    UserRepository userRepository;
 
     @GetMapping("/posts")
     public String index(Model model) {
@@ -36,6 +49,18 @@ public class PostsController {
     @PostMapping("/posts")
     public RedirectView create(@ModelAttribute Post post) {
         repository.save(post);
+        return new RedirectView("/posts");
+    }
+
+
+    @PostMapping("/comments")
+    public RedirectView postComment(@RequestParam Long postId, @RequestParam String content){
+
+        Comment comment = new Comment();
+        comment.setContent(content);
+        comment.setPostId(postId);
+        comment.setUserId(1L);
+        commentRepository.save(comment);
         return new RedirectView("/posts");
     }
 }
