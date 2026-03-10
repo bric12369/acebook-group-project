e560bb7172949a4c41c8e79cdeebcd7ff060c5d2
mahmudur mahid <m.mahid@icloud.com>
2026-03-10 14:27:28 +0000

    added base for likes model

---
+++ src/main/java/com/makersacademy/acebook/model/Like.java
@@ -0,0 +1,15 @@
+package com.makersacademy.acebook.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Data
+@Entity
+@Table(name="LIKES")
+@Getter @Setter @NoArgsConstructor
+public class Like {
+
+}
