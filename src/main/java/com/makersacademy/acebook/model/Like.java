b68839273e69eb1aea793ca5b4d77e22b9d7fa85
mahmudur mahid <m.mahid@icloud.com>
2026-03-10 14:29:08 +0000

    added like functionality to model

--- src/main/java/com/makersacademy/acebook/model/Like.java
+++ src/main/java/com/makersacademy/acebook/model/Like.java
@@ -11,5 +11,21 @@ import lombok.Setter;
 @Table(name="LIKES")
 @Getter @Setter @NoArgsConstructor
 public class Like {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    private Integer likes;
+
+    @Column(name = "post_id")
+    private Long postId;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    public Like(Integer likes, Long post_id, Long user_id) {
+        this.likes = likes;
+        this.postId = post_id;
+        this.userId = user_id;
+    }
 }
