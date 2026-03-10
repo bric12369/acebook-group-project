b7e0b2f107591823e0158907b9e94c51076d7237
mahmudur mahid <m.mahid@icloud.com>
2026-03-10 14:45:41 +0000

    debugging migration file

--- src/main/resources/db/migration/V4__create_likes_table.sql
+++ src/main/resources/db/migration/V4__create_likes_table.sql
@@ -4,6 +4,5 @@ CREATE TABLE likes(
     id bigserial PRIMARY KEY,
     post_id bigint REFERENCES posts(id) ON DELETE CASCADE,
     user_id bigint REFERENCES users(id) ON DELETE CASCADE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     UNIQUE(post_id, user_id)
 );
