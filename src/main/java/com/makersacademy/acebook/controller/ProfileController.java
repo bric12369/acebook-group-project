40ed4b59f75d7288f2af796226e695d1c82f2725
Alice Stiles <alicestileswebdev@gmail.com>
2026-03-11 15:11:50 +0000

    User profiles styles/template

--- src/main/java/com/makersacademy/acebook/controller/ProfileController.java
+++ src/main/java/com/makersacademy/acebook/controller/ProfileController.java
@@ -39,7 +39,7 @@ public class ProfileController {
         model.addAttribute("posts", posts);
         model.addAttribute("isCurrentUser", isCurrentUser);
 
-        return "profile/userprofile";
+        return "profile/profile";
 
     }
 }
