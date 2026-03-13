1227a10880c921fe5492fd7966330f90cfa21f0a
mollyrobbxo <molly.j.r@hotmail.co.uk>
2026-03-13 14:48:35 +0000

    search_for_user_functionality

--- src/main/java/com/makersacademy/acebook/repository/UserRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/UserRepository.java
@@ -3,8 +3,10 @@ package com.makersacademy.acebook.repository;
 import com.makersacademy.acebook.model.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends CrudRepository<User, Long> {
     public Optional<User> findUserByUsername(String username);
+    List<User> findByUsernameContainingIgnoreCase(String username);
 }
